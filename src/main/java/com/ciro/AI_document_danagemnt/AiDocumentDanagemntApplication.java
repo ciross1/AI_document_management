013d787072958be58919f63a34589eb1d88ac6e6
ciross1 <oricsantamarina@gmail.com>
2026-05-31 14:28:13 +0200

    Inital commit

---
+++ src/main/java/com/ciro/AI_document_danagemnt/AiDocumentDanagemntApplication.java
@@ -0,0 +1,13 @@
+package com.ciro.AI_document_danagemnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDocumentDanagemntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDocumentDanagemntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciro/AI_document_danagemnt/AiDocumentDanagemntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciro.AI_document_danagemnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDocumentDanagemntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
